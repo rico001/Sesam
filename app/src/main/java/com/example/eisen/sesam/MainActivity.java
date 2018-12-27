0e70180638c4d57ccb83d92b1cfd9dbec867c304
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2018-12-27 14:31:47 +0100

    Test -Commit1

--- app/src/main/java/com/example/eisen/sesam/MainActivity.java
+++ app/src/main/java/com/example/eisen/sesam/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.eisen.sesam;
-
+//TestKommentar für Commit
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
+
 }
