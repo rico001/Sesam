ca47d4dbd80c81bf2c404c9814415a21b22e1879
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2018-12-28 10:48:48 +0100

    "Allgemeine Einstellungen" hinzugefügt als erste Group in ExpandableLIst in activitymain.xml
    Sowie dessen Item ( Öfnungsdauer, Öffnen bei n Mal klingeln etc.)

---
+++ app/src/main/java/com/example/eisen/sesam/ExpandableListAdapter.java
@@ -0,0 +1,97 @@
+package com.example.eisen.sesam;
+
+import android.content.Context;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Context _context;
+    private ArrayList<String> _listDataHeader = new ArrayList<>();
+    private HashMap<String, List<String>> _listDataChild = new HashMap<>();
+
+    public ExpandableListAdapter(Context context) {
+        this._context = context;
+        _listDataHeader.add("Einstellungen");
+
+        List<String> top250 = new ArrayList<String>();
+        top250.add("Child1");
+
+        _listDataChild.put(_listDataHeader.get(0), top250);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .get(childPosititon);
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+
+        //&& childPosition==0!!!!!
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.general_settings_item,null);
+        }
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this._listDataHeader.get(groupPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this._listDataHeader.size();
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.general_settings_group, null);
+        }
+
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
--- app/src/main/java/com/example/eisen/sesam/MainActivity.java
+++ app/src/main/java/com/example/eisen/sesam/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.eisen.sesam;
 //TestKommentar für Commit
-//TestKommentar für Commit 2
+//TestKommentar für Commit 3
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ExpandableListView expListView;
+    ExpandableListAdapter listAdapterGeneralSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        listAdapterGeneralSettings = new ExpandableListAdapter(this);
+
+        expListView.setAdapter(listAdapterGeneralSettings);
     }
 
 }
